e754465f375ff1d9ede19fbeedc36d102b4bd663
Meredith Reece <mardebugger@gmail.com>
2020-08-13 01:18:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskList = document.getElementById("tasks");
+
+  document.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let newTask = document.getElementById("new-task-description");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(newTask.value));
+    taskList.appendChild(li);
+    e.target.reset();
+  })
 });
